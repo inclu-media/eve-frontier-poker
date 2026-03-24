49516c095f2800314e75034e455263387387bd8e
inclu-media <martin@inclumedia.at>
2026-03-24 20:40:40 +0100

    layout cleanup

--- dapps/src/App.tsx
+++ dapps/src/App.tsx
@@ -3,8 +3,8 @@ import { PokerTable } from "./PokerTable";
 
 function App() {
   return (
-    <Box style={{ background: "#050505", minHeight: "100vh", color: "#e0e0e0", position: "relative" }}>
-      <Flex direction="column" align="center" justify="center" style={{ minHeight: "100vh", width: "100%", padding: 0 }}>
+    <Box style={{ background: "#050505", minHeight: "100%", height: "100%", color: "#e0e0e0", position: "relative", display: "flex", flexDirection: "column" }}>
+      <Flex direction="column" align="center" justify="center" style={{ flex: 1, width: "100%", padding: 0 }}>
         <PokerTable />
       </Flex>
     </Box>
